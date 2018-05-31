9c54b44803e8817a48369a1546967e837fb947c0
kyriegorrin <kyriegorrin@gmail.com>
2018-05-31 10:06:15 +0000

    arrelada tabulació de mult1

--- Lab/Sessio5/matriu/mult1.c
+++ Lab/Sessio5/matriu/mult1.c
@@ -14,20 +14,18 @@ void mult1(int C[][N], int B[][N], int A[][N], int n)
 {
 
   int i,j,k, j0, k0, b;
-  b = 32/4;
+  b = 8;
   int sum;
   for ( j0 = 0; j0 < n; j0+= b){
-  for ( k0 = 0; k0 < n; k0+= b){
-  for ( i=0 ; i < n; i++ )
-  {
-    for ( j= j0 ; j < n && j < j0+b; j++ )
-    {
-      for ( k= k0 ; k < n && k < k0+b ; k++ )
-         C[i][j]+= A[i][k]*B[k][j];
-    }
+  	for ( k0 = 0; k0 < n; k0+= b){
+  		for ( i=0 ; i < n; i++ ){
+    		for ( j= j0 ; j < n && j < j0+b; j++ ){
+      			for ( k= k0 ; k < n && k < k0+b ; k++ )
+         			C[i][j]+= A[i][k]*B[k][j];
+    			}
+  		}			
+	}
   }
-}
-}
 }
 
 void init(int A[][N], int n)
