93763eb624178966ff13919f6035f80b8f12ba59
Ricard Zarco Badia <kyriegorrin@gmail.com>
2018-06-12 23:06:20 +0200

    Acabat (no testejat) exercici 7

--- Lab/Sessio6/rgb2yuv/rgb2yuv_7.c
+++ Lab/Sessio6/rgb2yuv/rgb2yuv_7.c
@@ -28,16 +28,45 @@ void rgb_to_yuv(t_pixel_rgb *rgb, t_pixel_yuv *yuv, int len, int N_iter)
 	int i,j;
 
 	//Declaracio de merdes auxiliars necessaries
-	float32x4_t vR, vG, vB, vY, vU, vV;	
+	float32x4_t vR, vG, vB, vY, vU, vV, vAux;	
 
 	//Matrix per vector product version
  	for (j=0; j<N_iter; j++){
 		for (i=0; i < len - 3; i+=4) {
+
+			//Carreguem els valors RGB
 			vR = vld1q_f32(&rgb->r[i]);
 			vG = vld1q_f32(&rgb->g[i]);
 			vB = vld1q_f32(&rgb->b[i]);
-			//TODO
 			
+			//Fem les operacions necessaries
+			//y[i]---y[i+3]
+			vY = vmulq_n_f32(vR, 0.299);
+			vAux = vmulq_n_f32(vG, 0.587);
+			vY = vaddq_f32(vY, vAux);
+			vAux = vmulq_n_f32(vB, 0.114);
+			vY = vaddq_f32(vY, vAux);
+						
+			//Fem les operacions necessaries
+			//u[i]---u[i+3]
+			vU = vmulq_n_f32(vR, -0.147);
+			vAux = vmulq_n_f32(vG, -0.289);
+			vU = vaddq_f32(vU, vAux);
+			vAux = vmulq_n_f32(vB, 0.436);
+			vU = vaddq_f32(vU, vAux);
+
+			//Fem les operacions necessaries
+			//v[i]---v[i+3]
+			vV = vmulq_n_f32(vR, 0.615);
+			vAux = vmulq_n_f32(vG, -0.515);
+			vV = vaddq_f32(vV, vAux);
+			vAux = vmulq_n_f32(vB, -0.100);
+			vV = vaddq_f32(vV, vAux);
+
+			//Carreguem valors a memoria de nou titu
+			vst1q_f32(&yuv->y[i], vY);	
+			vst1q_f32(&yuv->u[i], vU);	
+			vst1q_f32(&yuv->v[i], vV);	
 		}
 		for(; i < len; ++i){
 			UNROLL_RGB(i);
